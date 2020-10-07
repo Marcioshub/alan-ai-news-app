d574a96d32a417dc55c3684f24a81ccbec50c529
Marcio <mecastillo00@gmail.com>
2020-10-07 19:39:54 -0400

    Updates

---
+++ precache-manifest.3627aaa81c8d0aa9a4d41edfa5d21832.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fbc3abf588d4002f206dc8dbb2c35b6",
+    "url": "/alan-ai-news-app/index.html"
+  },
+  {
+    "revision": "ea1be2c40f591d037513",
+    "url": "/alan-ai-news-app/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "ecdb978ec7dae0d4f936",
+    "url": "/alan-ai-news-app/static/css/main.2ef04afc.chunk.css"
+  },
+  {
+    "revision": "ea1be2c40f591d037513",
+    "url": "/alan-ai-news-app/static/js/2.0de7d3ca.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/alan-ai-news-app/static/js/2.0de7d3ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecdb978ec7dae0d4f936",
+    "url": "/alan-ai-news-app/static/js/main.1b012de2.chunk.js"
+  },
+  {
+    "revision": "338f4b3a132cb22354c5",
+    "url": "/alan-ai-news-app/static/js/runtime-main.6bb9c086.js"
+  }
+]);
